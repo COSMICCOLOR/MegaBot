46d41df8f04310ed6b0630dcc7b1898f65c39839
COSMICCOLOR <dzikipm@gmail.com>
2023-05-22 21:21:13 +0300

    change 4

--- dima.py
+++ dima.py
@@ -140,13 +140,12 @@ def query_handler(call):
                              f" {dish_all_dict[call.data.split(':')[0]][6]}\nВремя "
                              f"приготовления: {dish_all_dict[call.data.split(':')[0]][4]} миунут!\n",
                              reply_markup=markup_dish)
-
     if call.data.split(':')[1] == "b3":
         bot.send_message(call.message.chat.id, "Выберите категорию", reply_markup=Sub_inline_keyb)
     if call.data.split(':')[1] == "txt2":
         Reviews_inline_keyb = InlineKeyboardMarkup()
-        Reviews_inline_keyb.add(InlineKeyboardButton("Наш сервис (отзывы)", callback_data="review:r1"))
-        Reviews_inline_keyb.add(InlineKeyboardButton("Наша еда (отзывы)", callback_data="review:r2"))
+        Reviews_inline_keyb.add(InlineKeyboardButton("Отзывы о сервисе", callback_data="review:r1"))
+        Reviews_inline_keyb.add(InlineKeyboardButton("Отзывы о еде", callback_data="review:r2"))
         Reviews_inline_keyb.add(InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
         bot.send_message(call.message.chat.id, "Выбирайте:", reply_markup=Reviews_inline_keyb)
         bot.send_message(call.message.chat.id, f"{gg}")
