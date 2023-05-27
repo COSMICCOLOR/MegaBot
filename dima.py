95019de8c8110b81e4e0ec56ec5c886383f909ed
COSMICCOLOR <dzikipm@gmail.com>
2023-05-28 00:01:35 +0300

    change 4

--- dima.py
+++ dima.py
@@ -4,12 +4,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import InputMediaPhoto
 import sqlite3
 import os
-import array
-import gspread
-import requests
-import subprocess
-import datetime
-import uuid
+import array, gspread, requests, subprocess, datetime, uuid
 
 # logfile = str(datetime.date.today()) + '.log' # формируем имя лог-файла
 token = '6112420224:AAFd0gDtUiAC2qqWo4osq82D6qyGH07c_UY'
@@ -84,7 +79,7 @@ with conn:
     orders_id = [i[4] for i in conn.execute(f"SELECT * FROM Reviews")]
     print("qqqqqqqqq", review_order, client_id)
     dish_id = [str(i[5]) for i in conn.execute(f"SELECT * FROM Reviews")]
-    review_order_dict = dict(zip(review_order[-3:-1], client_id[-3:-1]))  # можно корректировать индексами количество выводимых отзывов
+    review_order_dict = dict(zip(review_order[-3:], client_id[-3:]))  # можно корректировать индексами количество выводимых отзывов
     print(review_order_dict)
     review_dish_dict = dict(zip(dish_id, review_dish))
     print(review_dish_dict)
@@ -121,13 +116,35 @@ def create_keyboard():  # функция для создания клавиат�
     return markup_dish
 """***END Функция для создания клавиатуры с обновляемой кнопкой количества заказываемого блюда END***"""
 
+def create_edit_button():  # функция для создания кнопок для изменения полей профиля пользователя
+    edit_button = telebot.types.InlineKeyboardMarkup()
+    edit_button.add(telebot.types.InlineKeyboardButton("Изменить имя", callback_data="edit:name"))
+    edit_button.add(telebot.types.InlineKeyboardButton("Изменить телефон", callback_data="edit:phone_number"))
+    edit_button.add(telebot.types.InlineKeyboardButton("Изменить адрес", callback_data="edit:delivery_adress"))
+    edit_button.add(InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
+    return edit_button
+
+global reg_name, reg_phone_number, reg_delivery_adress  # значения регистрации по умолчанию
+reg_name = "Указать имя"
+reg_phone_number = "Указать телефон"
+reg_delivery_adress = "Указать адрес"
+def create_registration_keyb():  # функция для создания кнопок для регистрации пользователя
+    registration_keyb = telebot.types.InlineKeyboardMarkup(row_width=1)
+    registration_keyb.add(InlineKeyboardButton(reg_name, callback_data="reg:name"),
+                          InlineKeyboardButton(reg_phone_number, callback_data="reg:phone_number"),
+                          InlineKeyboardButton(reg_delivery_adress, callback_data="reg:delivery_adress"),
+                          InlineKeyboardButton("Сохранить\u2705", callback_data="accept:save_all"),
+                          InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
+    return registration_keyb
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, '''Добро пожаловать в чат-бот "FoodBot". Здесь Вы можете заказать еду по вкусу из ресторана "Літвіны".\nЧто Вас интересует?''', reply_markup=Main_inline_keyb)
     global user_telegram_id
     user_telegram_id = message.from_user.id
-    print(type(user_telegram_id), message.from_user.id)
+    # print(type(user_telegram_id), message.from_user.id)
+
 
 @bot.callback_query_handler(func=lambda call: call.data.split(":"))
 def query_handler(call):
@@ -163,42 +180,13 @@ def query_handler(call):
         Dish_inline_keyb.add(InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
         Dish_inline_keyb.add(InlineKeyboardButton("Назад", callback_data="menu:b3"))
         bot.send_message(call.message.chat.id, "Выберите категорию", reply_markup=Dish_inline_keyb)
-
-    # if call.data.split(':')[0] in dish_dict:
-    #     print(call.data.split(':')[0])
-    #     markup_dish = InlineKeyboardMarkup(row_width=3)
-    #     markup_dish.add(InlineKeyboardButton('<', callback_data='left'),
-    #                     InlineKeyboardButton('Кол-во', callback_data='None'),
-    #                     InlineKeyboardButton('>', callback_data='right'),
-    #                     InlineKeyboardButton('Заказать', callback_data='buy'),
-    #                     InlineKeyboardButton("В корзину", callback_data="basket"),)
-    #     if call.data.split(':')[0] in dish_all_dict:
-    #         # img = open(rf"C:\Users\admin\MegaBot\photo\{dish_all_dict[call.data.split(':')[0]][2]}", 'rb')
-    #         # bot.send_photo(call.message.chat.id, img)
-    #         bot.send_message(call.message.chat.id,
-    #                          f"{call.data.split(':')[0]}\nОписание:{dish_all_dict[call.data.split(':')[0]][1]}\nЦена: {dish_all_dict[call.data.split(':')[0]][3]}BYN (В упаковке вы увидите  {dish_all_dict[call.data.split(':')[0]][7]}шт.)\nВес:"
-    #                          f" {dish_all_dict[call.data.split(':')[0]][5]}"
-    #                          f" {dish_all_dict[call.data.split(':')[0]][6]}\nВремя "
-    #                          f"приготовления: {dish_all_dict[call.data.split(':')[0]][4]} миунут!\n",
-    #                          reply_markup=markup_dish)
-
-#START________________________________code serezha
-
+#START code serezha________________________________
     global dish_ids
     global dish_names
-
     if call.data.split(':')[0] in dish_dict:
         dish_ids = []
-        # markup_dish = InlineKeyboardMarkup(row_width=3)
-        # markup_dish.add(InlineKeyboardButton('-', callback_data='1:minus'),
-        #                 InlineKeyboardButton(str(count), callback_data='count'),
-        #                 InlineKeyboardButton('+', callback_data='2:plus'),
-        #                 InlineKeyboardButton("В корзину", callback_data="0:basket"),
-        #                 InlineKeyboardButton('Заказать', callback_data='3:buy'))
     if call.data.split(':')[0] in dish_all_dict:
         print("yyyyyyyyyyyy", call.data.split(':'), dish_all_dict[call.data.split(':')[0]][2])
-        # img = open(rf"C:\Users\admin\MegaBot\photo\{dish_all_dict[call.data.split(':')[0]][2]}", 'rb')
-        # bot.send_photo(call.message.chat.id, img)
         dish_ids.append(dish_all_dict[call.data.split(':')[0]][8])
         dish_names = dish_all_dict[call.data.split(':')[0]][0]
         global result_dish  # формируем карточку блюда и отправляем юзеру с клавиатурой для заказа и добавления в корзину
@@ -210,7 +198,6 @@ def query_handler(call):
         with open("photo/" + dish_all_dict[call.data.split(':')[0]][2], "rb") as img:  # calldata - id блюда и название соотв-й картинки этого блюда
             bot.send_photo(call.message.chat.id, photo=img)
         bot.send_message(call.message.chat.id, f"{result_dish}", reply_markup=create_keyboard())
-
     """***START Обработки колбэка от клавиатуры с обновляемой кнопкой количества заказываемого блюда START***"""
     global count  # используем глобальную переменную для количества добавляемого в корзину блюда
     bot.answer_callback_query(call.id)  # подтверждаем нажатие
@@ -224,7 +211,6 @@ def query_handler(call):
         bot.edit_message_text(f"{result_dish}", call.message.chat.id, call.message.message_id,
                               reply_markup=create_keyboard())  # обновляем сообщение с клавиатурой
     """***END Обработки колбэка от клавиатуры с обновляемой кнопкой количества заказываемого блюда END***"""
-
     global dict_info_dish_id
     client_id = int(call.message.chat.id)
     if call.data.split(':')[1] == 'basket':
@@ -234,7 +220,6 @@ def query_handler(call):
         with conn:
             conn.execute(cart, [client_id, dict_info_dish_id, 5.0])
         conn.commit()
-
     if call.data.split(':')[1] == 'buy':
         sravnenie_ids = [i[0] for i in cursor.execute(
             f'SELECT ShoppingCart.dish_id FROM ShoppingCart WHERE ShoppingCart.client_id = {client_id}')]
@@ -251,7 +236,7 @@ def query_handler(call):
                 total_price += j[4]
                 result += f'Блюдо: {j[1]}\n'
         bot.send_message(call.message.chat.id, f'{result} Цена:{total_price}')
-# END_______________________________________________________________________________________________code serezha
+# END code serezha_______________________________________________________________________________________________
     if call.data.split(':')[1] == "b3":
         bot.send_message(call.message.chat.id, "Выберите категорию", reply_markup=Sub_inline_keyb)
     """___Обработка коллбэка от кнопки 'О нас'___"""
@@ -341,7 +326,62 @@ def query_handler(call):
     #     ClientOrders_inline_keyb.add(InlineKeyboardButton("Назад", callback_data="menu:txt2"))
     #     bot.send_message(call.message.chat.id, "Вот все Ваши заказы. Выберите тот, на который хотите оставить отзыв:", reply_markup=ClientOrders_inline_keyb)
 
-
+    if call.data.split(':')[1] == "profile":
+        bot.answer_callback_query(call.id)
+        user_id = call.from_user.id  # id телеги пользователя
+        cursor.execute("SELECT * FROM Clients WHERE telegram_id = ?", (user_id,))  # проверяем, есть ли запись о пользователе в БД
+        row = cursor.fetchone()
+        if row is None:  # если нет, то просим пользователя ввести свои данные/зарегистрироваться
+            Reg_inline_keyb = InlineKeyboardMarkup()
+            Reg_inline_keyb.add(InlineKeyboardButton("РЕГИСТРАЦИЯ", callback_data="prereg:pushreg"))
+            Reg_inline_keyb.add(InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
+            bot.send_message(call.message.chat.id, "Чтобы пользоваться чат-ботом, нужно пройти регистрацию.", reply_markup=Reg_inline_keyb)
+        else:  # если есть, то показываем пользователю его текущие данные и предлагаем ему изменить их
+            name, phone, address = row[1], row[2], row[3]
+            bot.send_message(call.message.chat.id,
+                             f"Ваш профиль:\nИмя: {name}\nТелефон: {phone}\nАдрес: {address}\n\nВы можете изменить любое из этих полей, нажав на соответствующую кнопку.",
+                             reply_markup=create_edit_button())
+    global field, field_dict
+    field = call.data.split(':')[1]
+    field_dict = {"name": "Имя", "phone_number": "Телефон", "delivery_adress": "Адрес"}
+    if call.data.split(':')[0] == "edit":
+        bot.answer_callback_query(call.id)  # подтверждаем нажатие
+        bot.send_message(call.message.chat.id, f"Введите новое значение для поля '{field_dict[field]}'.", reply_markup=telebot.types.ForceReply())
+    global reg_name, reg_phone_number, reg_delivery_adress  # значения регистрации по умолчанию
+    if call.data.split(':')[1] == "pushreg":
+        with conn:
+            conn.execute("INSERT OR IGNORE INTO Clients (name, phone_number, delivery_adress, telegram_id) values(?, ?, ?, ?)", (reg_name, reg_phone_number, reg_delivery_adress, user_telegram_id))
+        conn.commit()
+        bot.answer_callback_query(call.id)  # подтверждаем нажатие
+        bot.send_message(call.message.chat.id, "Заполните все поля формы и сохраните данные:", reply_markup=create_registration_keyb())
+    if call.data.split(':')[0] == "reg":
+        bot.answer_callback_query(call.id)  # подтверждаем нажатие
+        global reg_field, reg_field_dict
+        reg_field = call.data.split(':')[1]  # получаем название поля для изменения
+        reg_field_dict = {"name": "Имя", "phone_number": "Телефон", "delivery_adress": "Адрес"}
+        bot.send_message(call.message.chat.id, f"Введите значение для поля '{reg_field_dict[reg_field]}'.", reply_markup=telebot.types.ForceReply())
+    if call.data.split(':')[0] == "accept":
+        # save_field = call.data.split(':')[1]
+        # print("1", reg_name)
+        # print("1", reg_phone_number)
+        # print("1", reg_delivery_adress)
+        with conn:
+            conn.execute("INSERT OR IGNORE INTO Clients (name, phone_number, delivery_adress, telegram_id) values(?, ?, ?, ?)", (reg_name, reg_phone_number, reg_delivery_adress, user_telegram_id))
+        conn.commit()
+        # bot.send_message(call.message.chat.id, f"Данные для поля '{reg_field_dict[save_field]}' успешно сохранены.", reply_markup=create_registration_keyb())
+        if reg_name != "Указать имя" and reg_phone_number != "Указать телефон" and len(reg_phone_number) == 13 and reg_delivery_adress != "Указать адрес":
+            Success_reg_inline_keyb = InlineKeyboardMarkup()
+            Success_reg_inline_keyb.add(InlineKeyboardButton("Вернуться в меню", callback_data="menu:b1"))
+            bot.answer_callback_query(call.id)
+            bot.send_message(call.message.chat.id, f"Вы успешно прошли регистрацию.\n"
+                                                   f"Ваш профиль:\n"
+                                                   f"Имя: {reg_name}\n"
+                                                   f"Телефон: {reg_phone_number}\n"
+                                                   f"Адрес: {reg_delivery_adress}\n\n"
+                                                   f"Вы можете изменить любое из этих полей в своём профиле.",
+                             reply_markup=Success_reg_inline_keyb)
+        else:
+            bot.send_message(call.message.chat.id, f"Введите корректные данные для всех полей и сохраните данные.", reply_markup=create_registration_keyb())
 
 
 # обработка ответа пользователя на вопрос о его отзыве на работу ресторана с последующей запись в БД
@@ -359,10 +399,31 @@ def handle_review_answer(message):
     conn.commit()  # сохраняем изменения в базе данных
     bot.send_message(message.chat.id, "Спасибо за Ваш отзыв!", reply_markup=Main_inline_keyb)
 
+# обрабатываем ответ пользователя на вопрос о его имени пользователя/телефоне/адресе {field} при их изменении
+@bot.message_handler(func=lambda message: message.reply_to_message and message.reply_to_message.text in [f"Введите новое значение для поля '{field_dict[field]}'."])
+def handle_name_answer(message):
+    # message.from_user.id - это telegram id, message.text - введенные пользователем данные
+    with conn:
+        # добавляем новую запись в таблицу "Clients" с id телеги и именем пользователя/телефоном/адресом {field}
+        conn.execute(f"UPDATE Clients SET {field} = ? WHERE telegram_id = ?", (message.text, message.from_user.id))
+    conn.commit()
+    bot.send_message(message.chat.id, f"Вы успешно изменили {field_dict[field]}.", reply_markup=create_edit_button())
 
-
-
-
+# обрабатываем ответы пользователя при регистрации на вопросы о его имени пользователя/телефоне/адресе reg_field_dict[reg_field]
+@bot.message_handler(func=lambda message: message.reply_to_message and message.reply_to_message.text in [f"Введите значение для поля '{reg_field_dict[reg_field]}'.", f"Введите корректные данные для всех полей и сохраните данные."])
+def handle_reg_answer(message):
+    global reg_name, reg_phone_number, reg_delivery_adress  # значения регистрации по умолчанию
+    # print("2", reg_name)
+    # print("2", reg_phone_number)
+    # print("2", reg_delivery_adress)
+    # print(reg_field)
+    if reg_field == 'name':
+        reg_name = message.text
+    if reg_field == 'phone_number':
+        reg_phone_number = message.text
+    if reg_field == 'delivery_adress':
+        reg_delivery_adress = message.text
+    bot.send_message(message.chat.id, f'''Вы успешно изменили поле "{reg_field_dict[reg_field]}".\nЗаполните все поля формы и сохраните данные:''', reply_markup=create_registration_keyb())
 
 print("Ready")
 bot.infinity_polling(none_stop=True, interval=0)
